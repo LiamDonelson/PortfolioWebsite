0e0ab4bf81224ce826c3a2be40d805e1d3900a97
unknown <liamsdonelson@gmail.com>
2023-04-28 00:49:00 -0400

    Added home page layout along with color and design. Made a Sidenav, but buttons dont work yet!

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,32 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
-import { NavigationBarComponent } from 'src/nav-bar/nav-bar.component';
+import { NavBarComponent } from 'src/nav-bar/nav-bar.component';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatListModule} from '@angular/material/list';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button'
+
+
+
+
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavigationBarComponent
+    NavBarComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NoopAnimationsModule,
+    MatToolbarModule,
+    MatSidenavModule,
+    MatListModule,
+    MatIconModule,
+    MatButtonModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
