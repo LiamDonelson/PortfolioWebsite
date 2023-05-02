b0cda3c3389500aabc3b0b828bfc5202b72dc95c
unknown <liamsdonelson@gmail.com>
2023-05-02 18:20:03 -0400

    Adding more to the sidenav, fixing some issues and implementing a picture

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,7 +11,16 @@ import {MatButtonModule} from '@angular/material/button'
 
 
 
-
+export declare interface OnInit {
+    /**
+     * A callback method that is invoked immediately after the
+     * default change detector has checked the directive's
+     * data-bound properties for the first time,
+     * and before any of the view or content children have been checked.
+     * It is invoked only once when the directive is instantiated.
+     */
+    ngOnInit(): void;
+}
 
 
 @NgModule({
@@ -31,4 +40,8 @@ import {MatButtonModule} from '@angular/material/button'
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule { 
+
+
+
+}
